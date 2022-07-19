3ef02c80186ba1c462816ec7583d922b9ef9e4b2
christianadouglas3842 <105051226+christianadouglas3842@users.noreply.github.com>
2022-07-19 20:14:55 +0300

    Update path.go

--- path.go
+++ path.go
@@ -6,7 +6,6 @@ package objabi
 
 import "strings"
 
-// PathToPrefix converts raw string to the prefix that will be used in the
 // symbol table. All control characters, space, '%' and '"', as well as
 // non-7-bit clean bytes turn into %xx. The period needs escaping only in the
 // last segment of the path, and it makes for happier users if we escape that as
@@ -36,14 +35,11 @@ func PathToPrefix(s string) string {
 			p = append(p, c)
 		}
 	}
-
-	return string(p)
 }
 
 // IsRuntimePackagePath examines 'pkgpath' and returns TRUE if it
 // belongs to the collection of "runtime-related" packages, including
 // "runtime" itself, "reflect", "syscall", and the
-// "runtime/internal/*" packages. The compiler and/or assembler in
 // some cases need to be aware of when they are building such a
 // package, for example to enable features such as ABI selectors in
 // assembly sources.
